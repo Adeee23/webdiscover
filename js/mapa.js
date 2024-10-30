c42d737438999aa86afabd8f400b77910eba9022
Adeee23 <adect93@gmail.com>
2024-10-30 15:18:53 +0100

    Primer commit

--- js/mapa.js
+++ js/mapa.js
@@ -1,11 +1,17 @@
 // Inicializamos el mapa en la ubicación central de la empresa
-let map = L.map("mapa").setView([40.32319, -3.63424], 12);
+let map = L.map("mapa").setView([39.8544, -4.0231], 7);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // Agregamos un marcador en la ubicación de la empresa
-let markerEmpresa = L.marker([40.32319, -3.63424]).addTo(map).bindPopup('Nuestra ubicación').openPopup();
+let markerEmpresa = L.marker([39.8544, -4.0231]).addTo(map).bindPopup('DISCOVER').openPopup();
+
+// Marcador por defecto en la ubicación del cliente
+let markerCliente = L.marker([38.872505, -6.971732]).addTo(map).bindPopup('MASTER D').openPopup();
+
+// Variable para almacenar la ruta actual
+let rutaActual;
 
 // Función para geocodificar la dirección del cliente y calcular la ruta
 function calcularRuta() {
@@ -27,18 +33,26 @@ function calcularRuta() {
                 let latCliente = data[0].lat;
                 let lonCliente = data[0].lon;
 
-                // Marcador para la ubicación del cliente
-                let markerCliente = L.marker([latCliente, lonCliente]).addTo(map).bindPopup('Tu ubicación').openPopup();
+                // Actualizamos el marcador del cliente a la nueva ubicación ingresada
+                map.removeLayer(markerCliente); // Eliminamos el marcador anterior
+                markerCliente = L.marker([latCliente, lonCliente]).addTo(map).bindPopup('Tu ubicación').openPopup();
 
-                // Configuramos Leaflet Routing Machine para calcular la ruta entre el cliente y la empresa
-                L.Routing.control({
+                // Si ya hay una ruta trazada, la eliminamos
+                if (rutaActual) {
+                    map.removeControl(rutaActual);
+                }
+
+                // Configuramos Leaflet Routing Machine para calcular la nueva ruta
+                rutaActual = L.Routing.control({
                     waypoints: [
                         L.latLng(latCliente, lonCliente),  // Coordenadas del cliente
-                        L.latLng(40.32319, -3.63424)       // Coordenadas de la empresa
+                        L.latLng(39.8544, -4.0231)         // Coordenadas de la empresa
                     ],
                     routeWhileDragging: true
                 }).addTo(map);
 
+                // Movemos el mapa para mostrar la nueva ubicación del cliente y la ruta
+                map.setView([latCliente, lonCliente], 10);
             } else {
                 alert("No se pudo encontrar la ubicación. Intenta con otra dirección.");
             }
